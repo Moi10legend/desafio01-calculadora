0bdd524a17fdab0e23c25e4fb48e39572cc4fad1
Moi10legend <moisesflopesf@gmail.com>
2024-06-27 10:17:05 -0300

    projeto finalizado

---
+++ src/components/buttons/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #CDCDCD;
+
+    font-size: 24px;
+    flex: 1;
+
+    &:hover{
+        background-color: gray;
+        color: white;
+    }
+`
---
+++ src/components/input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #FFAAAA;
+    border-radius: 10px 10px 0 0;
+
+    display: flex;
+    align-items: center;
+    
+
+    font-size: 24px;
+    font-family: 'Arial';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        border-radius: 10px 10px 0 0;
+        padding: 0 10px;
+        color: white;
+        text-align: right;
+
+        font-size: 24px;
+        font-family: 'Arial';
+        
+    }
+`
